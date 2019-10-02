ed3a89e0ff3c816d6a501611d4bf8c853eb9cdf1
Tomczyki <dawid41.1993@gmail.com>
2019-10-02 21:21:35 +0200

    Adding first, main view

--- src/main/java/pl/sda/quiz_app/Controllers/QuizController.java
+++ src/main/java/pl/sda/quiz_app/Controllers/QuizController.java
@@ -1,4 +1,28 @@
 package pl.sda.quiz_app.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import pl.sda.quiz_app.Repositories.QuizRepository;
+
+@Controller
 public class QuizController {
+
+    private QuizRepository quizRepository;
+
+    @Autowired
+    public QuizController(QuizRepository quizRepository) {
+        this.quizRepository = quizRepository;
+    }
+
+    @GetMapping
+    public ModelAndView getQuizes(){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("quizes");
+        mav.addObject("quizes",quizRepository.findAll());
+        return  mav;
+
+    }
+
 }
