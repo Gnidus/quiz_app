f2fd76135da86f6a1974e45ecbb0cb2ab42d7f6e
Tomczyki <dawid41.1993@gmail.com>
2019-10-07 20:12:04 +0200

    Viewing all question from quiz and new view for score (TODO)

--- src/main/java/pl/sda/quiz_app/Controllers/QuizController.java
+++ src/main/java/pl/sda/quiz_app/Controllers/QuizController.java
@@ -3,10 +3,13 @@ package pl.sda.quiz_app.Controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import pl.sda.quiz_app.Exceptions.QuizNotFoundException;
 import pl.sda.quiz_app.Models.Question;
+import pl.sda.quiz_app.Models.Quiz;
 import pl.sda.quiz_app.Repositories.QuizRepository;
 
 @Controller
@@ -33,6 +36,23 @@ public class QuizController {
         mav.setViewName("quiz");
         Question questionById = quizRepository.findById(id).orElseThrow(QuizNotFoundException::new).getByIndex(questionIndex);
         mav.addObject("questionById",questionById);
+        return mav;
+    }
+
+    @PostMapping("quiz{id}/question/{questionIndex}")
+    public String postAnswers(@PathVariable("id") Integer id, @PathVariable("questionIndex") Integer questionIndex){
+        if ((questionIndex+1)>quizRepository.findById(id).orElseThrow(QuizNotFoundException::new).getQuestionList().size()){
+            return "redirect:/quiz"+id+"/score";
+        }else {
+            return "redirect:/quiz"+id+"/question/"+ (questionIndex +1);
+        }
+    }
+    @GetMapping("quiz{id}/score")
+    public ModelAndView getScore(@PathVariable("id") Integer id){
+        ModelAndView mav = new ModelAndView();
+        Quiz quizById = quizRepository.findById(id).orElseThrow(QuizNotFoundException::new);
+        mav.setViewName("score");
+        mav.addObject("quizById",quizById);
         return mav;
     }
 
